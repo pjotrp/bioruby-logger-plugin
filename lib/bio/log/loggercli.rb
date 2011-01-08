8199d750e8558e043c5e81ae9cfef94a06acdaa1
Pjotr Prins <pjotr.public01@thebird.nl>
2011-01-08 10:26:51 +0100

    Parsing CLI

--- lib/bio/log/loggercli.rb
+++ lib/bio/log/loggercli.rb
@@ -1,7 +1,38 @@
+require 'singleton'
 
 module Bio
   module Log
-    class CLI
+
+    class LoggerPlusGlobal
+      include Singleton
+
+      attr_accessor :outputter_type, :trace
+
+      def initialize
+      end
+ 
+    end
+
+    module CLI
+      # Parse and store global logger type
+      def CLI::logger name
+        LoggerPlusGlobal.instance.outputter_type = case name
+          when 'stderr' then :stderr
+          when 'stdout' then :stdout
+          else 
+            {:file => { :filename => name }}
+        end
+      end
+
+      # Parse and store trace options
+      def CLI::trace s
+        opts = {}
+        type = :default
+        level, sub_level = s.split(':')
+        sub_level = sub_level.to_i if sub_level 
+        opts[type] = { :level => level, :sub_level => sub_level }
+        LoggerPlusGlobal.instance.trace = opts
+      end
     end
   end
 end
--- spec/biologger_cli_spec.rb
+++ spec/biologger_cli_spec.rb
@@ -16,13 +16,29 @@ describe Bio::Log::CLI, "bio-logger command line parsing" do
     # @mylog = Bio::Log::LoggerPlus.root
     @mylog = LoggerPlus.new 'mylog'
     @mylog.outputters = Outputter.stderr
+    @global = LoggerPlusGlobal.instance
   end
 
-  it "should parse --logger stderr"
-  it "should parse --logger filen to add filename logger"
-  it "should parse --trace  debug to show all messages"
-  it "should parse --trace  warn to show how messages more serious than 'warn'"
-  it "should parse --trace  warn:3 to show messaged more serious that 'warn' level 3"
+  it "should parse --logger stderr and set the global" do
+    CLI.logger("stderr")
+    LoggerPlusGlobal.instance.outputter_type.should == :stderr
+  end
+  it "should parse --logger filen to set filename logger" do
+    CLI.logger("test.log")
+    @global.outputter_type[:file].should == {:filename => 'test.log'}
+  end
+  it "should parse --trace  debug to show all messages" do
+    CLI.trace("debug")
+    @global.trace[:default].should == { :level => 'debug', :sub_level => nil } 
+  end
+  it "should parse --trace  warn to show how messages more serious than 'warn'" do
+    CLI.trace("warn")
+    @global.trace[:default].should == { :level => 'warn', :sub_level => nil } 
+  end
+  it "should parse --trace  warn:3 to show messaged more serious that 'warn' level 3" do
+    CLI.trace("warn:3")
+    @global.trace[:default].should == { :level => 'warn', :sub_level => 3 } 
+  end
   it "should parse --trace  gff3:info:5 to override level for 'gff3' to info level 5"
   it "should parse --trace  blast:debug to override level for 'blast'"
   it "should parse --trace  blast,gff3:debug to override level for 'blast' and 'gff3'"
